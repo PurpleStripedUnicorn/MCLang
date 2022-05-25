ca1f68fe4f82a11e0bb3ee255cbdb3e2f8cf3292
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-05-25 17:24:12 +0200

    #3 - Implemented main functions of the lexer

--- mclang/lexer/lexer.cc
+++ mclang/lexer/lexer.cc
@@ -1,6 +1,85 @@
 
 #include "lexer/lexer.h"
 
-Lexer::Lexer(std::string inp) : txt(inp) {
+Lexer::Lexer(std::string inp) : txt(inp), curIndex(0), atLineStart(true) {
 
+}
+
+void Lexer::next() {
+    curIndex++;
+}
+
+bool Lexer::atEnd() const {
+    return curIndex >= txt.size();
+}
+
+char Lexer::cur() const {
+    if (atEnd())
+        return '$';
+    return txt[curIndex];
+}
+
+std::vector<Token> Lexer::readIn() {
+    curIndex = 0;
+    Token tok;
+    std::vector<Token> out;
+    while (!atEnd()) {
+        // Ignore whitespace
+        if (cur() == ' ' || cur() == '\t') {
+            ;
+        } else if (cur() == '\n') {
+            atLineStart = true;
+        } else if (readInToken(tok)) {
+            out.push_back(tok);
+            atLineStart = false;
+        } else {
+            // TODO: Add proper error handling
+            std::cout << "Could not recognize token!" << std::endl;
+            return out;
+        }
+    }
+    return out;
+}
+
+bool Lexer::readInToken(Token &tok) {
+    if (cur() == '(') {
+        tok = Token(TOK_LBRACE, "");
+        return true;
+    } else if (cur() == ')') {
+        tok = Token(TOK_RBRACE, "");
+        return true;
+    } else if (cur() == '{') {
+        tok = Token(TOK_LCBRACE, "");
+        return true;
+    } else if (cur() == '}') {
+        tok = Token(TOK_RCBRACE, "");
+        return true;
+    } else if (cur() == '/' && atLineStart) {
+        tok = Token(TOK_RCBRACE, "");
+        return true;
+    } else if (cur() == '}') {
+        tok = Token(TOK_RCBRACE, "");
+        return true;
+    } else if (('a' <= cur() && cur() <= 'z') || ('A' <= cur() && cur() <= 'Z'))
+    {
+        return readInWord(tok);
+    }
+    return false;
+}
+
+bool Lexer::readInWord(Token &tok) {
+    std::string word = "";
+    while (('a' <= cur() && cur() <= 'z') || ('A' <= cur() && cur() <= 'Z')
+    || cur() == '_' || ('0' <= cur() && cur() <= '9')) {
+        word.push_back(cur());
+        next();
+    }
+    if (word == "")
+        return false;
+    // Detect typenames and special words
+    TokenType tt = TOK_WORD;
+    if (word == "int" || word == "void")
+        tt = TOK_TYPENAME;
+    tok = Token(tt, word);
+    return true;
 }
--- mclang/lexer/lexer.h
+++ mclang/lexer/lexer.h
@@ -2,7 +2,10 @@
 #ifndef __LEXER_H__
 #define __LEXER_H__
 
+#include "lexer/token.h"
+#include <iostream>
 #include <string>
+#include <vector>
 
 class Lexer {
 
@@ -14,10 +17,58 @@ public:
      */
     Lexer(std::string inp);
 
+    /**
+     * Move to the next character
+     * @post `curIndex` is incremented
+     */
+    void next();
+
+    /**
+     * Check if the lexer is at the end of the input string
+     * @return Boolean indicating if the current index is past the end of the
+     * input string
+     */
+    bool atEnd() const;
+
+    /**
+     * Get the current character
+     * @return The character at the `curIndex` position in `txt`
+     */
+    char cur() const;
+
+    /**
+     * Read in the stored string and convert it to tokens
+     * @return A vector of tokens generated from the `txt` string
+     */
+    std::vector<Token> readIn();
+
+    /**
+     * Read in one token
+     * @param tok Reference to a token object where the found token will be
+     * placed, if any is found
+     * @return Boolean indicating if a token could be read
+     */
+    bool readInToken(Token &tok);
+
+    /**
+     * Read in a word, typename, etc.
+     * @param tok Reference to a token object where the fond token will be
+     * placed, if any is found
+     * @return Boolean indicating if a token could be read
+     */
+    bool readInWord(Token &tok);
+
 private:
+
+    // Index of the current character being read
+    unsigned int curIndex;
 
     // String to be tokenized
     std::string txt;
+
+    // Indicates wether we're still reading the start of a line (excluding
+    // spaces/tabs)
+    bool atLineStart;
 
 };
 
