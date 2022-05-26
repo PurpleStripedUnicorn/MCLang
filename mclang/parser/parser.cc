ffa75a47eb535adca608066e586f73fd12ba33b3
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-05-26 14:33:22 +0200

    #4 - Implemented parse nodes in the main parser

--- mclang/parser/parser.cc
+++ mclang/parser/parser.cc
@@ -1,2 +1,79 @@
 
 #include "parser/parser.h"
+
+Parser::Parser(const std::vector<Token> &toks) : toks(toks), curIndex(0),
+out(NULL) {
+
+}
+
+Parser::~Parser() {
+    delete out;
+}
+
+ParseNode *Parser::genTree() {
+    curIndex = 0;
+    delete out;
+    ParseNode *out = readInProgram();
+    if (curIndex != toks.size())
+        // TODO: Implement better error handling
+        std::cout << "Stopped reading before EOF" << std::endl;
+    return out;
+}
+
+Token Parser::cur() const {
+    if (curIndex >= toks.size())
+        return Token(TOK_ERRTYPE);
+    return toks[curIndex];
+}
+
+void Parser::next() {
+    curIndex++;
+}
+
+bool Parser::accept(TokenType type) const {
+    return cur().type == type;
+}
+
+void Parser::expect(TokenType type) {
+    if (!accept(type))
+        std::cout << "Expected a different token type! Located at line "
+        << cur().loc.line << "[" << cur().loc.col << "]" << std::endl;
+}
+
+ParseNode *Parser::readInProgram() {
+    std::vector<ParseNode *> childNodes;
+    while (accept(TOK_TYPENAME))
+        childNodes.push_back(readInFunc());
+    return new ProgramNode(childNodes);
+}
+
+ParseNode *Parser::readInFunc() {
+    expect(TOK_TYPENAME);
+    if (cur().content != "void")
+        std::cout << "Invalid typename for function, has to be 'void'"
+        << std::endl;
+    next();
+    expect(TOK_WORD);
+    std::string name = cur().content;
+    next();
+    expect(TOK_LBRACE), next();
+    expect(TOK_RBRACE), next();
+    expect(TOK_LCBRACE), next();
+    CodeBlockNode *codeblock = (CodeBlockNode *)readInCodeBlock();
+    expect(TOK_RCBRACE), next();
+    return new FuncNode(name, codeblock);
+}
+
+ParseNode *Parser::readInCodeBlock() {
+    std::vector<ParseNode *> childNodes;
+    while (!accept(TOK_RCBRACE))
+        childNodes.push_back(readInCmd());
+    return new CodeBlockNode(childNodes);
+}
+
+ParseNode *Parser::readInCmd() {
+    expect(TOK_CMD);
+    ParseNode *out = new CmdNode(cur().content);
+    next();
+    return out;
+}
--- mclang/parser/parser.h
+++ mclang/parser/parser.h
@@ -2,14 +2,101 @@
 #ifndef __PARSER_H__
 #define __PARSER_H__
 
+#include "lexer/token.h"
+#include "parsenodes/cmd.h"
+#include "parsenodes/codeblock.h"
+#include "parsenodes/func.h"
+#include "parsenodes/parsenode.h"
+#include "parsenodes/program.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
 class Parser {
 
 public:
 
     /**
      * Constructor
+     * @param toks Vector of input tokens
      */
-    Parser();
+    Parser(const std::vector<Token> &toks);
+
+    /**
+     * Destructor
+     * @note Destroys the generated trees (if any exist)
+     */
+    ~Parser();
+
+    /**
+     * Construct a parse tree from the stored tokens
+     * @return A pointer to the generated parse tree root node
+     * @post Parse tree output pointer is stored in `out` variable
+     * @warning Do not delete output tree pointer! This will be deleted on
+     * destruction of the parser object!
+     */
+    ParseNode *genTree();
+
+private:
+
+    /**
+     * Get the current token being read
+     * @return The token in `toks[curIndex]`
+     */
+    Token cur() const;
+
+    /**
+     * Move to the next token
+     * @post `curIndex` is incremented
+     */
+    void next();
+
+    /**
+     * Check if the current token is of the given type
+     * @param type The token type
+     * @return Boolean indicating if the types match
+     */
+    bool accept(TokenType type) const;
+
+    /**
+     * Indicate that a certain token type is expected, shows an error if the
+     * type does not match with the current token type
+     * @param type The token type
+     */
+    void expect(TokenType type);
+
+    /**
+     * Read in an entire program
+     * @return A pointer to the generated parse node
+     */
+    ParseNode *readInProgram();
+
+    /**
+     * Read in a function definition
+     * @return A pointer to the generated parse node
+     */
+    ParseNode *readInFunc();
+
+    /**
+     * Read in a code block
+     * @return A pointer to the generated parse node
+     */
+    ParseNode *readInCodeBlock();
+
+    /**
+     * Read in an inserted command
+     * @return A pointer to the generated parse node
+     */
+    ParseNode *readInCmd();
+
+    // Vector of input tokens
+    const std::vector<Token> &toks;
+
+    // Current index of the token being read
+    unsigned int curIndex;
+
+    // Pointer to output parse tree
+    ParseNode *out;
 
 };
 
