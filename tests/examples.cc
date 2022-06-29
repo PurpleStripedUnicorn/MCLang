cf3461c7c7ab7abd67dfa32b886d372f1eb4721f
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-06-29 19:26:28 +0200

    #48 - Created a C++ test file

--- tests/examples.cc
+++ tests/examples.cc
@@ -1,7 +1,36 @@
-
+/**
+ * @warning This script is only to be executed by someone with exprience with
+ * programming, execution of this script may be dangerous on some systems!
+ */
 #include <iostream>
+#include <filesystem>
+#include <string>
 
 int main () {
-    std::cout << "Hello World!" << std::endl;
+    unsigned int success = 0, failed = 0;
+    std::string out = "";
+    for (auto const &entry : std::filesystem::directory_iterator("examples")) {
+        if (entry.is_regular_file()) {
+            std::string path = entry.path();
+            if (path.find("invalid") != std::string::npos)
+                continue;
+            if (system(("build/main -D " + path).c_str()) == 0) {
+                out += "\033[0;32mSUCCESS:    \033[0m" + path + "\n";
+                success++;
+            } else {
+                out += "\033[0;31mFAILURE:    \033[0m" + path + "\n";
+                failed++;
+            }
+        }
+    }
+    std::cout << std::endl << out << std::endl;
+    if (failed > 0) {
+        std::cout << "\033[0;31m" << failed << " failures, " << success
+        << " successes\n\033[0m" << std::endl;
+        return 1;
+    } else {
+        std::cout << failed << " failures, " << success << " successes"
+        << std::endl;
+    }
     return 0;
 }
