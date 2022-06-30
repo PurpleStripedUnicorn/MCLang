1b679deef17e07e7d756e096c36cbfd8c13a04b4
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-06-30 12:14:54 +0200

    #51 - Created function call parse node

---
+++ mclang/parsenodes/call.cc
@@ -0,0 +1,24 @@
+
+#include "bcgen/bcgen.h"
+#include "bcgen/instr.h"
+#include "parsenodes/call.h"
+#include "parsenodes/parsenode.h"
+#include <string>
+#include <vector>
+
+CallNode::CallNode(std::string fname, ParseNodeProps props) :
+ParseNode(PNODE_CMD, props), fname(fname) {
+
+}
+
+CallNode::~CallNode() {
+    
+}
+
+std::vector<ParseNode *> CallNode::children() const {
+    return {};
+}
+
+void CallNode::bytecode(BCManager &man) const {
+    // TODO: implement bytecode generation
+}
---
+++ mclang/parsenodes/call.h
@@ -0,0 +1,45 @@
+
+#ifndef __PARSENODE_CMD_H__
+#define __PARSENODE_CMD_H__
+
+#include "bcgen/bcgen.h"
+#include "parsenodes/parsenode.h"
+#include <string>
+#include <vector>
+
+class CallNode : public ParseNode {
+
+public:
+
+    /**
+     * Constructor
+     * @param fname The name of the function to call
+     * @param props General parse node properties
+     */
+    CallNode(std::string fname, ParseNodeProps props);
+
+    /**
+     * Destructor
+     */
+    virtual ~CallNode() override;
+
+    /**
+     * Get the children of this parse node
+     * @return A vector with pointers to the child nodes
+     */
+    virtual std::vector<ParseNode *> children() const override;
+
+    /**
+     * Generate bytecode for this parse node
+     * @param man The main bytecode manager
+     */
+    virtual void bytecode(BCManager &man) const override;
+
+private:
+
+    // The name of the function to call
+    std::string fname;
+
+};
+
+#endif
