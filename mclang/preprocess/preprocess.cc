78df49f8053a1c3966ae4625a284243be3105333
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-07-06 12:11:52 +0200

    #62 - Implemented some preliminary ideas for creating preprocessor

--- mclang/preprocess/preprocess.cc
+++ mclang/preprocess/preprocess.cc
@@ -1,11 +1,90 @@
 
+#include "errorhandle/handle.h"
 #include "preprocess/preprocess.h"
 #include <string>
+#include <fstream>
 
-Preprocessor::Preprocessor() : curRead(""), includeDepth(0) {
+InpStream::InpStream(std::string filename) : stream(filename),
+filename(filename), line(1), col(0) {
+
+}
+
+InpStream::~InpStream() {
+    stream.close();
+}
+
+char InpStream::get() {
+    char out = stream.get();
+    if (out == '\r')
+        out = stream.get();
+    col++;
+    if (out == '\n')
+        line++, col = 0;
+    return out;
+}
+
+char InpStream::peek() {
+    char out;
+    out = stream.peek();
+    if (out == '\r') {
+        stream.get();
+        out = stream.peek();
+    }
+    return out;
+}
+
+bool InpStream::good() {
+    return stream.good() && stream.is_open();
+}
+
+Preprocessor::Preprocessor() : curRead(""), includeDepth(0), doOutput(true) {
 
 }
 
 Preprocessor::~Preprocessor() {
 
+}
+
+void Preprocessor::processFile(std::string filename) {
+    InpStream fs(filename);
+    readCodeBlock(fs);
+}
+
+void Preprocessor::readCodeBlock(InpStream &fs) {
+    
+}
+
+Preprocessor::PrepLine Preprocessor::readLine(InpStream &fs) {
+    PrepLine out;
+    char cur;
+    bool atLineStart = true;
+    bool isCmd = false;
+    bool inCmdWord = false;
+    std::string cmdWord = "";
+    fs.stream.get(cur);
+    while (fs.stream.good() && fs.stream.is_open() && cur != '\n') {
+        out.content.push_back(cur);
+        if (atLineStart) {
+            if (cur == '#') {
+                isCmd = true;
+                inCmdWord = true;
+            } else if (cur != ' ' && cur != '\t') {
+                atLineStart = false;
+            }
+        }
+        if (inCmdWord) {
+            if (cur == ' ') {
+                inCmdWord = false;
+                out.content = "";
+            } else {
+                cmdWord.push_back(cur);
+            }
+        }
+        fs.stream.get(cur);
+    }
+    if (cmdWord == "include")
+        out.type = LINE_INCL;
+    else
+        MCLError(1, "Invalid preprocessor statement");
+    
 }
--- mclang/preprocess/preprocess.h
+++ mclang/preprocess/preprocess.h
@@ -2,10 +2,64 @@
 #ifndef __PREPROCESS_H__
 #define __PREPROCESS_H__
 
+#include "preprocess/codetext.h"
+#include <fstream>
 #include <string>
 #include <vector>
 
 #define MAX_INCLUDE_DEPTH 128
+
+/**
+ * Wrapper around input file stream objects, used to keep track of information
+ * like line and column numbers
+ */
+class InpStream {
+
+public:
+
+    /**
+     * Constructor
+     * @param filename The file to be read
+     */
+    InpStream(std::string filename);
+
+    /**
+     * Destructor
+     */
+    ~InpStream();
+
+    /**
+     * Get a character from the stream
+     * @return The character read
+     * @post `stream`, `line` and `col` are changed accordingly
+     */
+    char get();
+
+    /**
+     * Peek to see the next character that will be read with get
+     * @return The character to be read next
+     */
+    char peek();
+
+    /**
+     * Check if the end of the file has been reached or the stream has exited
+     * @return Boolean indicating that reading can continue and there are still
+     * characters to be read
+     */
+    bool good();
+
+    // Raw input filestream
+    std::ifstream stream;
+
+private:
+
+    // Filename, for convenience
+    std::string filename;
+
+    // Current line and column of the stream
+    unsigned int line, col;
+
+};
 
 class Preprocessor {
 
@@ -21,7 +75,47 @@ public:
      */
     ~Preprocessor();
 
+    /**
+     * Process a given file
+     * @param filename The file to process
+     * @post The output is appended in `out`, if needed
+     * @note Processes included files recursively
+     */
+    void processFile(std::string filename);
+
 private:
+
+    /**
+     * Command type, or just raw text
+     */
+    enum PrepLineType {
+        LINE_TXT, LINE_INCL
+    };
+
+    /**
+     * Used internally when reading lines
+     * Content is just the entire line when dealing with raw text, or anything
+     * that comes after the statement when dealing with a preprocessor statement
+     */
+    struct PrepLine {
+        PrepLineType type = LINE_TXT;
+        std::string content = "";
+    };
+
+    /**
+     * Read in a preprocessor code block
+     * @param fs The current input filestream
+     * @post The output is appended in `out`, if needed
+     */
+    void readCodeBlock(InpStream &fs);
+
+    /**
+     * Read in a single line. Depending on wether it is raw text or a
+     * preprocessor statement, an action is taken
+     * @param fs The current input filestream
+     * @note Skips over the endline at the end
+     */
+    PrepLine readLine(InpStream &fs);
 
     // The line that has been read up to this point
     std::string curRead;
@@ -29,7 +123,12 @@ private:
     // Current include depth: number of includes that are above the current file
     // being read
     unsigned int includeDepth;
-    
+
+    // Output switch
+    bool doOutput;
+
+    // Output
+    CodeText out;
 
 };
 
