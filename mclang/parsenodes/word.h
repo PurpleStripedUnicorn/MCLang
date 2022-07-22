540959db5f0e137f3797f6906f10d086de04b8fc
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-22 19:25:57 +0200

    #27 - Changed pure virtual function in word node to non-pure

--- mclang/parsenodes/word.h
+++ mclang/parsenodes/word.h
@@ -34,7 +34,7 @@ public:
      * Generate bytecode for this parse node
      * @param man The main bytecode manager
      */
-    virtual void bytecode(BCManager &man) const = 0;
+    virtual void bytecode(BCManager &man) const override;
 
     /**
      * Get the content of the word
