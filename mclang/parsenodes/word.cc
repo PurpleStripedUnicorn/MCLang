31f0567676e339ca1cdc3d983a217968c4bd9386
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-22 18:22:26 +0200

    #27 - Added word parse node

---
+++ mclang/parsenodes/word.cc
@@ -0,0 +1,27 @@
+
+#include "bcgen/bcgen.h"
+#include "errorhandle/handle.h"
+#include "parsenodes/parsenode.h"
+#include "parsenodes/word.h"
+
+WordNode::WordNode(std::string content, ParseNodeProps props) :
+ParseNode(PNODE_WORD, props) {
+
+}
+
+WordNode::~WordNode() {
+
+}
+
+std::vector<ParseNode *> WordNode::children() const {
+    return {};
+}
+
+void WordNode::bytecode(BCManager &man) const {
+    // Check if the variable is actually defined
+    if (!man.varManager.hasVar(content))
+        MCLError(1, "Accessing uninitialized variable \"" + content + "\"",
+        props.loc.line, props.loc.col);
+    // Return the variable value to "__res"
+    man.write(BCInstr(INSTR_COPY, "__res", content));
+}
