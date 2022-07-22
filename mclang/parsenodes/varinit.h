b76a16c088f41f8438cb60bf888d5bffb6a7cc37
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-22 18:27:26 +0200

    #27 - Added variable init node

---
+++ mclang/parsenodes/varinit.h
@@ -0,0 +1,54 @@
+
+#ifndef __PARSENODE_VARINIT_H__
+#define __PARSENODE_VARINIT_H__
+
+#include "parsenodes/parsenode.h"
+#include <vector>
+#include <string>
+
+class BCManager;
+class ExprNode;
+
+class VarInitNode : public ParseNode {
+
+public:
+
+    /**
+     * Constructor
+     * @param varType The variable type to make the variable
+     * @param childExpr Child node of the initialization, which should be an
+     * assignment, or just the name of the variable
+     * @param props General parse node properties
+     */
+    VarInitNode(std::string varType, ParseNode *childExpr,
+    ParseNodeProps props);
+
+    /**
+     * Destructor
+     */
+    virtual ~VarInitNode() override;
+
+    /**
+     * Get the children of this parse node
+     * @return A vector with pointers to the child nodes
+     */
+    virtual std::vector<ParseNode *> children() const override;
+
+    /**
+     * Generate bytecode for this parse node
+     * @param man The main bytecode manager
+     */
+    virtual void bytecode(BCManager &man) const override;
+
+protected:
+
+    // The type of the initialized variable
+    std::string varType;
+
+    // The child expression of this node, which should be an assignment
+    // expression
+    ParseNode *childExpr;
+
+};
+
+#endif
