302046e091f2744373f523c8144a9bbbe2d11232
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-23 14:59:08 +0200

    #75 - Implemented Loc in lexer

--- mclang/lexer/lexer.cc
+++ mclang/lexer/lexer.cc
@@ -22,14 +22,12 @@ void Lexer::readIn() {
         Loc lastLoc = cur().loc;
         if (readInToken(tok)) {
             if (tok.type != TOK_EMPTY) {
-                tok.loc.col = lastLoc.col;
-                tok.loc.line = lastLoc.line;
+                tok.loc = lastLoc;
                 readTokens.push_back(tok);
             }
         // Token not recognized
         } else {
-            MCLError(1, "Could not recognize token.", lastLoc.line, lastLoc.col
-            );
+            MCLError(1, "Could not recognize token.", lastLoc);
         }
     }
 }
@@ -47,8 +45,12 @@ inline bool Lexer::atEnd() const {
 }
 
 PrepToken Lexer::cur() const {
-    if (atEnd())
-        return PrepToken(PTOK_EOF, Loc::unknown);
+    if (atEnd()) {
+        Loc lastLoc = Loc::unknown;
+        if (input.size() > 0)
+            lastLoc = input.back().loc;
+        return PrepToken(PTOK_EOF, lastLoc);
+    }
     return input[curIndex];
 }
 
