3777c193cc99a64e08c9c74b83627ef81458807e
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-23 14:34:41 +0200

    #75 - Added more functionality to Loc

--- mclang/general/loc.cc
+++ mclang/general/loc.cc
@@ -1,9 +1,16 @@
 
 #include "general/loc.h"
+#include <string>
+
+Loc::Loc(unsigned int line, unsigned int col) : filename(Loc::nofile),
+line(line), col(col) {
+
+}
 
 Loc::Loc(std::string filename, unsigned int line, unsigned int col) :
 filename(filename), line(line), col(col) {
 
 }
 
-const Loc Loc::unknown = Loc("??", 0, 0);
+const Loc Loc::unknown = Loc(Loc::nofile, 0, 0);
+const std::string Loc::nofile = "??";
