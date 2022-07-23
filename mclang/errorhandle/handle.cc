e3f825ade298152e1a3953a34549e20db487f077
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-23 14:50:54 +0200

    #75 - Expanded error handler and changed look of errors

--- mclang/errorhandle/handle.cc
+++ mclang/errorhandle/handle.cc
@@ -1,43 +1,34 @@
 
 #include "errorhandle/colors.h"
 #include "errorhandle/handle.h"
+#include "general/loc.h"
 #include <iostream>
 #include <string>
 
 MCLError::MCLError(unsigned int level, std::string txt, unsigned int line,
-unsigned int col) : level(level), txt(txt), loc({line, col}) {
+unsigned int col) : level(level), txt(txt), loc(Loc(line, col)) {
+    print();
+}
+
+MCLError::MCLError(unsigned int level, std::string txt, Loc loc) : level(level),
+txt(txt), loc(loc) {
     print();
 }
 
 void MCLError::print() const {
-    std::cerr << std::endl;
-    for (unsigned int i = 0; i < LEFT_PAD; i++)
-        std::cerr << " ";
-    unsigned int errtxtSize = 0;
-    if (level == 0) {
+    std::string locText = loc.filename;
+    if (loc.line == 0)
+        locText += ":??";
+    else
+        locText += ":" + std::to_string(loc.line);
+    if (loc.col != 0 && loc.line != 0)
+        locText += ":" + std::to_string(loc.col);
+    colorText(std::cerr, locText + ": ", "white bold");
+    if (level == 0)
         colorText(std::cerr, "warning:", "magenta bold");
-        errtxtSize = 8;
-    } else {
+    else
         colorText(std::cerr, "error:", "red bold");
-        errtxtSize = 6;
-    }
-    for (unsigned int i = 0; i + errtxtSize < ERRTXT_PAD; i++)
-        std::cerr << " ";
-    std::string lineTxt = "";
-    if (loc.line != 0 || loc.col != 0)
-        lineTxt = "Line " + std::to_string(loc.line) + "["
-        + std::to_string(loc.col) + "]";
-    while (lineTxt.size() < LINETXT_PAD)
-        lineTxt.push_back(' ');
-    std::cerr << lineTxt;
-    std::cerr << txt.substr(0, TXT_PAD);
-    for (unsigned int i = TXT_PAD; i < txt.size(); i += TXT_PAD) {
-        std::cerr << std::endl;
-        for (unsigned int j = 0; j < LEFT_PAD + ERRTXT_PAD + LINETXT_PAD; j++)
-            std::cerr << " ";
-        std::cerr << txt.substr(i, TXT_PAD);
-    }
-    std::cerr << std::endl << std::endl;
+    std::cerr << std::endl << "    " << txt << std::endl;
     if (level == 1)
         exit(1);
 }
--- mclang/errorhandle/handle.h
+++ mclang/errorhandle/handle.h
@@ -2,12 +2,8 @@
 #ifndef __ERRORHANDLE_H__
 #define __ERRORHANDLE_H__
 
+#include "general/loc.h"
 #include <string>
-
-#define LEFT_PAD 3
-#define ERRTXT_PAD 10
-#define LINETXT_PAD 15
-#define TXT_PAD 60
 
 /**
  * Error object, shows the error/warning/note on creation
@@ -27,6 +23,15 @@ public:
     MCLError(unsigned int level, std::string txt, unsigned int line = 0,
     unsigned int col = 0);
 
+    /**
+     * Constructor
+     * @param level The level of seriousness, a level of 0 means the program
+     * will not be halted and indicates a "warning", level 1 is an "error"
+     * @param txt The error text
+     * @param loc The location of error, which also includes file information
+     */
+    MCLError(unsigned int level, std::string txt, Loc loc);
+
 private:
 
     /**
@@ -41,9 +46,7 @@ private:
     std::string txt;
 
     // Location of the error
-    struct {
-        unsigned int line, col;
-    } loc;
+    Loc loc;
 
 };
 
