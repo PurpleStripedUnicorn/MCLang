dfbed0ea4c27aa6b2f45b9a5044157d238ba9d27
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-26 11:29:09 +0200

    #86 - Added comments

--- mclang/general/types.h
+++ mclang/general/types.h
@@ -2,10 +2,16 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+/**
+ * Basic type
+ */
 enum BaseType {
     TYPE_VOID, TYPE_INT, TYPE_BOOL
 };
 
+/**
+ * Extended type, which also contains info about being const
+ */
 struct Type {
     BaseType base;
     bool isConst;
