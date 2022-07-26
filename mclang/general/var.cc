503d6dfe0391659c7624fa8c02d799aace9a61e0
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-26 11:45:26 +0200

    #86 - Added Type to Var and ConstVar

--- mclang/general/var.cc
+++ mclang/general/var.cc
@@ -1,11 +1,11 @@
 
 #include "general/var.h"
 
-Var::Var(std::string type, std::string name) : type(type), name(name) {
+Var::Var(Type type, std::string name) : type(type), name(name) {
 
 }
 
-ConstVar::ConstVar(std::string type, std::string name, std::string value) :
+ConstVar::ConstVar(Type type, std::string name, std::string value) :
 Var(type, name), value(value) {
     
 }
--- mclang/general/var.h
+++ mclang/general/var.h
@@ -2,21 +2,23 @@
 #ifndef __VARIABLE_H__
 #define __VARIABLE_H__
 
+#include "general/types.h"
 #include <string>
 
 /**
  * A variable with type and possible constant value
  */
 struct Var {
-    Var(std::string type, std::string name);
-    std::string type, name;
+    Var(Type type, std::string name);
+    Type type;
+    std::string name;
 };
 
 /**
  * A constant variable, with value attached
  */
 struct ConstVar : public Var {
-    ConstVar(std::string type, std::string name, std::string value);
+    ConstVar(Type type, std::string name, std::string value);
     std::string value;
 };
 
