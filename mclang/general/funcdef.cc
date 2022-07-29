8aaa67d4f9aeb6cb5358dc37aac9983833f34a52
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-29 17:57:11 +0200

    #83 - Removed old FuncDef and FuncAlias

--- mclang/general/funcdef.cc
+++ mclang/general/funcdef.cc
@@ -6,27 +6,3 @@
 Param::Param(Type type, std::string name) : type(type), name(name) {
 
 }
-
-FuncAlias::FuncAlias() : name("??"), constValues({}) {
-
-}
-
-FuncAlias::FuncAlias(std::string name) : name(name), constValues({}) {
-
-}
-
-FuncAlias::FuncAlias(std::string name, std::vector<std::string> constValues) :
-name(name), constValues(constValues) {
-
-}
-
-FuncDef::FuncDef() : name("??"), params({}) {
-    
-}
-
-FuncDef::FuncDef(std::string name, std::vector<Param> params) : name(name),
-params(params) {
-
-}
-
-const FuncDef FuncDef::unknown = FuncDef("??", {});
