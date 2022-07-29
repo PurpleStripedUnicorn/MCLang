d01eadabb55fc3953c8114b7494a28f655d3f08d
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-07-29 11:17:48 +0200

    #83 - Moved structs outside class

--- mclang/bcgen/funcman.cc
+++ mclang/bcgen/funcman.cc
@@ -1,6 +1,11 @@
 
 #include "bcgen/funcman.h"
 #include "bcgen/bcgen.h"
+#include "parsenodes/func.h"
+#include "parsenodes/parsenode.h"
+#include <map>
+#include <string>
+#include <vector>
 
 FuncManager::FuncManager(BCManager &bcman) : bcman(bcman) {
 
--- mclang/bcgen/funcman.h
+++ mclang/bcgen/funcman.h
@@ -8,6 +8,37 @@
 #include <vector>
 
 class BCManager;
+class FuncNode;
+class ParseNode;
+
+/**
+ * Function alias
+ * This is a bytecode function that can be called, that has the correct
+ * const values filled in inside the code
+ */
+struct FuncAlias {
+    std::string callname;
+    std::vector<std::string> constValues;
+};
+
+/**
+ * Function parameter
+ * Contains the type and the parameter name
+ */
+struct FuncParam {
+    Type type;
+    std::string name;
+};
+
+/**
+ * Registered function type, contains information about aliases and input
+ * types
+ */
+struct Func {
+    std::string name;
+    std::vector<FuncParam> params;
+    std::vector<FuncAlias> aliases;
+};
 
 /**
  * Function manager
@@ -29,39 +60,16 @@ public:
      */
     ~FuncManager();
 
+    /**
+     * Register a given function
+     * @param node The function parse node
+     */
+    void addFunc(FuncNode *node);
+
 private:
 
     // Main bytecode manager
     BCManager &bcman;
-
-    /**
-     * Function alias
-     * This is a bytecode function that can be called, that has the correct
-     * const values filled in inside the code
-     */
-    struct FuncAlias {
-        std::string callname;
-        std::vector<std::string> constValues;
-    };
-
-    /**
-     * Function parameter
-     * Contains the type and the parameter name
-     */
-    struct FuncParam {
-        Type type;
-        std::string name;
-    };
-
-    /**
-     * Registered function type, contains information about aliases and input
-     * types
-     */
-    struct Func {
-        std::string name;
-        std::vector<FuncParam> params;
-        std::vector<FuncAlias> aliases;
-    };
 
     // List of registered functions, also containing aliases
     std::map<std::string, std::vector<Func>> funcs;
