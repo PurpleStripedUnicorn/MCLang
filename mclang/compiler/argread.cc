123dae8981c031b912c833619734cf4363360f23
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-08-01 14:42:29 +0200

    #113 - Implemented ArgReader and added it to main.cc

--- mclang/compiler/argread.cc
+++ mclang/compiler/argread.cc
@@ -2,6 +2,7 @@
 #include "compiler/argread.h"
 #include "compiler/compiler.h"
 #include "errorhandle/handle.h"
+#include <iostream>
 #include <string>
 
 ArgReader::ArgReader(Compiler *comp) : comp(comp), curIndex(0), argc(0),
@@ -14,7 +15,7 @@ ArgReader::~ArgReader() {
 }
 
 void ArgReader::read(int argc, char *argv[]) {
-    curIndex = 0;
+    curIndex = 1;
     this->argc = argc;
     this->argv = argv;
     hasGivenFilename = false;
@@ -24,9 +25,19 @@ void ArgReader::read(int argc, char *argv[]) {
         MCLError(1, "No filename given.");
 }
 
+std::string ArgReader::execName() const {
+    if (argc < 1)
+        exit(1);
+    return std::string(argv[0]);
+}
+
+void ArgReader::invalidError() const {
+    MCLError(1, "Invalid command line arguments.");
+}
+
 std::string ArgReader::cur() const {
     if (curIndex >= argc)
-        return "";
+        invalidError();
     return std::string(argv[curIndex]);
 }
 
@@ -47,5 +58,110 @@ void ArgReader::readCur() {
 
 void ArgReader::readMainArg() {
     comp->filename = cur();
+    hasGivenFilename = true;
+    next();
+}
+
+void ArgReader::readLetterArg() {
+    if (cur().size() != 2)
+        invalidError();
+    char letter = cur()[1];
+    next();
+    if (letter == 'c')
+        readDescription();
+    else if (letter == 'd')
+        readDebug();
+    else if (letter == 'D')
+        readDisableOutput();
+    else if (letter == 'n')
+        readNamespace();
+    else if (letter == 'h')
+        readHelp();
+    else if (letter == 'o')
+        readOutput();
+    else if (letter == 'v')
+        readVersion();
+}
+
+void ArgReader::readWordArg() {
+    if (cur().size() <= 2)
+        invalidError();
+    std::string word = cur().substr(2);
+    next();
+    if (word == "custom-description")
+        readDescription();
+    else if (word == "debug")
+        readDebug();
+    else if (word == "disable-output")
+        readDisableOutput();
+    else if (word == "namespace")
+        readNamespace();
+    else if (word == "help")
+        readHelp();
+    else if (word == "output")
+        readOutput();
+    else if (word == "scoreboard")
+        readScoreboard();
+    else if (word == "version")
+        readVersion();
+}
+
+void ArgReader::readDescription() {
+    comp->description = cur();
+    next();
+}
+
+void ArgReader::readDebug() {
+    comp->debugMode = true;
+}
+
+void ArgReader::readDisableOutput() {
+    comp->fileOutput = false;
+}
+
+void ArgReader::readNamespace() {
+    comp->ns = cur();
+    next();
+}
+
+void ArgReader::readHelp() {
+    std::cout << std::endl << "Command usage: " << execName()
+    << " <input filename> [<arguments>]" << std::endl << std::endl;
+    for (unsigned int i = 0; i < sizeof(argList) / sizeof(ArgHelpText); i++) {
+        std::string cur = "";
+        if (argList[i].letter != ' ')
+            cur.push_back('-'), cur.push_back(argList[i].letter);
+        if (argList[i].letter != ' ' && argList[i].word != "")
+            cur.append(", ");
+        if (argList[i].word != "")
+            cur.append("--" + argList[i].word);
+        while (cur.size() < HELPPADLEFT)
+            cur.push_back(' ');
+        cur.append(argList[i].description.substr(0, HELPPADRIGHT));
+        for (unsigned int j = HELPPADRIGHT; j < argList[i].description.size();
+        j += HELPPADRIGHT) {
+            cur.push_back('\n');
+            for (unsigned int k = 0; k < HELPPADLEFT; k++)
+                cur.push_back(' ');
+            cur.append(argList[i].description.substr(j, HELPPADRIGHT));
+        }
+        std::cout << cur << std::endl;
+    }
+    std::cout << std::endl;
+    exit(0);
+}
+
+void ArgReader::readOutput() {
+    comp->outputFolder = cur();
+    next();
+}
+
+void ArgReader::readScoreboard() {
+    comp->scoreboardName = cur();
+    next();
+}
+
+void ArgReader::readVersion() {
+    comp->mcVersion = cur();
     next();
 }
--- mclang/compiler/argread.h
+++ mclang/compiler/argread.h
@@ -2,9 +2,42 @@
 #ifndef __ARGREAD_H__
 #define __ARGREAD_H__
 
+#define HELPPADLEFT 25
+#define HELPPADRIGHT 100
+
 #include <string>
+#include <vector>
 
 class Compiler;
+
+/**
+ * Help text for a certain argument, containing the letter argument (' ' for no
+ * letter argument), the word argument ("" for no word argument) and the
+ * description of the argument
+ */
+struct ArgHelpText {
+    char letter;
+    std::string word;
+    std::string description;
+};
+
+const ArgHelpText argList[] = {
+    {'c', "custom-description", "Set the output datapack description"},
+    {'d', "debug", "Use debugging tools. Debug info will be dumped in files in "
+    "the current working directory."},
+    {'D', "disable-output", "Disable result output to the file system. Will "
+    "even disable output when \"-o\" is given."},
+    {'n', "namespace", "Change the namespace of the output datapack, default "
+    "is 'dp'"},
+    {'h', "help", "Show the help page."},
+    {'o', "output", "Set the output folder, default is 'out_datapack'. "
+    "Warning: this folder will be overwritten by this program! Be very careful "
+    "when selecting an output folder!"},
+    {' ', "scoreboard", "Set the scoreboard objective used for variables. "
+    "Default is \"mclang\"."},
+    {'v', "version", "Set the Minecraft version to output as a datapack. Only "
+    "supports normal versions after 1.17. Default is \"latest\""}
+};
 
 /**
  * Object to read in command line arguments given by "-" and "--" arguments and
@@ -36,9 +69,20 @@ public:
 private:
 
     /**
+     * Get the executable filename
+     * @return The (entered) executable filename as a string
+     */
+    std::string execName() const;
+
+    /**
+     * Show an error for invalid command line arguments
+     */
+    void invalidError() const;
+
+    /**
      * Get the current command line argument as a string
-     * @return `argv[curIndex]` converted to a string, returns an empty string
-     * if `curIndex` is larger than or equal to `argc`
+     * @return `argv[curIndex]` converted to a string
+     * @warning Shows an error if `curIndex` is larger than or equal to `argc`
      */
     std::string cur() const;
 
@@ -67,6 +111,46 @@ private:
      * Read in a word size argument, so starting with "--"
      */
     void readWordArg();
+
+    /**
+     * Read in the "description" command line argument
+     */
+    void readDescription();
+
+    /**
+     * Read in the "debug" command line argument
+     */
+    void readDebug();
+
+    /**
+     * Read in the "disable output" command line argument
+     */
+    void readDisableOutput();
+
+    /**
+     * Read in the "namespace" command line argument
+     */
+    void readNamespace();
+
+    /**
+     * Read in the "help" command line argument
+     */
+    void readHelp();
+
+    /**
+     * Read in the "output" command line argument
+     */
+    void readOutput();
+
+    /**
+     * Read in the "scoreboard" command line argument
+     */
+    void readScoreboard();
+
+    /**
+     * Read in the "version" command line argument
+     */
+    void readVersion();
 
     // The main compiler component
     Compiler *comp;
