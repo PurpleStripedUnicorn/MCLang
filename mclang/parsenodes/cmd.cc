655bd975110eaae7c6cab369d99b52290daa3908
PurpleStripedUnicorn <purplestripedunicorn@users.noreply.github.com>
2022-08-02 16:33:18 +0200

    #95 - Implemented constant insertion

--- mclang/parsenodes/cmd.cc
+++ mclang/parsenodes/cmd.cc
@@ -23,6 +23,7 @@ std::vector<ParseNode *> CmdNode::children() const {
 }
 
 void CmdNode::bytecode(BCManager &man) {
+    replaceConstants(man);
     if (primaryCmd() == "function")
         MCLError(0, "Raw function call insertion has undefined behaviour, use "
         "normal function call instead!", loc);
@@ -32,7 +33,7 @@ void CmdNode::bytecode(BCManager &man) {
     if (!isKnownCmd())
         MCLError(0, "The given command \"" + primaryCmd() + "\" is not known "
         "to be a valid command.", loc);
-    man.write(BCInstr(INSTR_CMD, cmd));
+    man.write(BCInstr(INSTR_CMD, processedCmd));
     man.ret.type = Type("void");
     man.ret.value = "";
 }
@@ -44,7 +45,41 @@ bool CmdNode::isKnownCmd() const {
 std::string CmdNode::primaryCmd() const {
     std::string out;
     unsigned int i = 0;
-    while (i < cmd.size() && cmd[i] != ' ')
-        out.push_back(cmd[i]), i++;
+    while (i < cmd.size() && processedCmd[i] != ' ')
+        out.push_back(processedCmd[i]), i++;
     return out;
+}
+
+void CmdNode::replaceConstants(const BCManager &man) {
+    std::string out = "";
+    unsigned int i = 0;
+    while (i < cmd.size()) {
+        if (i + 1 < cmd.size() && cmd[i] == '{' && cmd[i + 1] == '{')
+            i += 2, out.append(readInConstant(man, i));
+        else
+            out.push_back(cmd[i]), i++;
+    }
+    processedCmd = out;
+}
+
+std::string CmdNode::readInConstant(const BCManager &man, unsigned int &i)
+const {
+    std::string constName = "";
+    while (i < cmd.size() && isAlphaNumUS(cmd[i]))
+        constName.push_back(cmd[i]), i++;
+    if (i >= cmd.size() || cmd[i] != '}')
+        return constName;
+    i++;
+    if (i >= cmd.size() || cmd[i] != '}')
+        return constName + "}";
+    i++;
+    // Search for constant name definition
+    std::string value = man.ctx.getConstValue(constName);
+    if (value == "")
+        return constName + "}}";
+    return value;
+}
+
+bool CmdNode::isAlphaNumUS(char c) const {
+    return c == '_' || ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z');
 }
